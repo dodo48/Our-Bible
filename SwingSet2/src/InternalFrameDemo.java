f49902e1508ba661c5cc801243115d2749689089
DD <DD@PC-08>
2017-09-10 13:47:06 +0300

    ещё изменения

--- SwingSet2/src/InternalFrameDemo.java
+++ SwingSet2/src/InternalFrameDemo.java
@@ -126,6 +126,7 @@ public class InternalFrameDemo extends DemoModule {
 
         // Create four more starter windows
         // privet!
+        // ещё изменения
         //222 createInternalFrame(icon1, DEMO_FRAME_LAYER, FRAME_WIDTH, FRAME_HEIGHT);
         createInternalFrame(icon3, DEMO_FRAME_LAYER, FRAME_WIDTH, FRAME_HEIGHT);
         createInternalFrame(icon4, DEMO_FRAME_LAYER, FRAME_WIDTH, FRAME_HEIGHT);
